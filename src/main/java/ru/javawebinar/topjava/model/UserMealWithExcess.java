ebd31b6941a65e60fee60cfeb5211fa3119e6f62
undina <foxundina@gmail.com>
2021-06-03 14:24:50 +0300

    Revert "Apply 1_0_rename.patch"
    This reverts commit 420dd261

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -2,7 +2,7 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class MealTo {
+public class UserMealWithExcess {
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -11,7 +11,7 @@ public class MealTo {
 
     private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -20,7 +20,7 @@ public class MealTo {
 
     @Override
     public String toString() {
-        return "MealTo{" +
+        return "UserMealWithExcess{" +
                 "dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
